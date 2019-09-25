33f926de2d0981fbca0c420c4886b1ededb1cea3
Kimaru thagana <macbuntu@localhost.localdomain>
2019-09-25 20:27:21 +0300

    extract username and location

--- extract_tweets.py
+++ extract_tweets.py
@@ -13,6 +13,14 @@ api.update_status("Look, I'm tweeting from #Python in my #earthanalytics class! 
 search_words = "#wildfires"
 date_since = "2018-11-16"
 
+new_search = search_words + " -filter:retweets" # remove retweets
+new_search_2= "climate+change -filter:retweets"
+
+'''
+Sometimes you may want to remove retweets as they contain duplicate content that might skew your analysis 
+if you are only looking at word frequency.
+'''
+
 # Collect tweets
 tweets = tw.Cursor(api.search,
               q=search_words,
@@ -23,3 +31,9 @@ tweets = tw.Cursor(api.search,
 # Iterate and print tweets
 for tweet in tweets:
     print(tweet.text)
+
+# who is tweeting about a certain topic and from where
+users_locs = [[tweet.user.screen_name, tweet.user.location] for tweet in tweets]
+# create a dataframe
+tweet_text = pd.DataFrame(data=users_locs,
+                    columns=['user', "location"])
