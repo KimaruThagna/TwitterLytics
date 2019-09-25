b3f20b0aad4b4111531502fb602c256d6f88e5ff
Kimaru thagana <macbuntu@localhost.localdomain>
2019-09-25 20:22:17 +0300

    collect and print tweets with a search term

--- extract_tweets.py
+++ extract_tweets.py
@@ -8,4 +8,18 @@ api = tw.API(auth, wait_on_rate_limit=True)
 
 # Post a tweet from Python
 api.update_status("Look, I'm tweeting from #Python in my #earthanalytics class! @EarthLabCU")
-# Your tweet has been posted!
+
+# Define the search term and the date_since date as variables
+search_words = "#wildfires"
+date_since = "2018-11-16"
+
+# Collect tweets
+tweets = tw.Cursor(api.search,
+              q=search_words,
+              lang="en",
+              since=date_since).items(5)
+
+
+# Iterate and print tweets
+for tweet in tweets:
+    print(tweet.text)
