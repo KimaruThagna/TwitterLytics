4692394921e7bc0610d2f8e1d4e4ef57e7b4ebbc
Kimaru thagana <macbuntu@localhost.localdomain>
2019-09-26 21:33:49 +0300

    derive most common words from tweets

--- word_freq_analysis.py
+++ word_freq_analysis.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import itertools
 import collections
-
 import tweepy as tw
 import nltk
 from nltk.corpus import stopwords
@@ -35,3 +34,27 @@ def remove_url(txt):
 
 all_tweets_no_urls = [remove_url(tweet) for tweet in all_tweets]
 print(f'tweets with no urls {all_tweets_no_urls[:5]}')
+
+# Create a list of lists containing lowercase words for each tweet
+words_in_tweet = [tweet.lower().split() for tweet in all_tweets_no_urls]
+print(f'>>>>>>>>>>>>>>>\n{words_in_tweet[:2]}')
+
+# List of all words across tweets
+all_words_no_urls = list(itertools.chain(*words_in_tweet))
+
+# Create counter
+counts_no_urls = collections.Counter(all_words_no_urls)
+
+print(f'>>>>>>>>15 most common words {counts_no_urls.most_common(15)}')
+
+clean_tweets_no_urls = pd.DataFrame(counts_no_urls.most_common(15),
+                             columns=['words', 'count'])
+
+print(clean_tweets_no_urls.head())
+
+# Download stopwords
+nltk.download('stopwords')
+stop_words = set(stopwords.words('english'))
+
+# View a few words from the set
+print(f'View the first 10 stopwords {list(stop_words)[0:10]}')
