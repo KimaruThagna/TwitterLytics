23d5b041733ea35dcca56438314723f4e5200004
Kimaru thagana <macbuntu@localhost.localdomain>
2019-09-26 09:27:50 +0300

    remove urls from tweets

--- word_freq_analysis.py
+++ word_freq_analysis.py
@@ -10,9 +10,28 @@ import nltk
 from nltk.corpus import stopwords
 import re
 import networkx
-
+from extract_tweets import api
 import warnings
 warnings.filterwarnings("ignore")
 
 sns.set(font_scale=1.5)
 sns.set_style("whitegrid")
+
+search_term = "#climate+change -filter:retweets"
+
+tweets = tw.Cursor(api.search,
+                   q=search_term,
+                   lang="en",
+                   since='2018-11-01').items(1000)
+
+all_tweets = [tweet.text for tweet in tweets]
+
+print(f' Sample tweets \n {all_tweets[:5]}')
+
+# use regular expressions to remove irrelevant urls first part of the regex removes symbols. The second part removes urls in the format
+# http or https:// ((any url form))
+def remove_url(txt):
+    return " ".join(re.sub("([^0-9A-Za-z \t])|(\w+:\/\/\S+)", "", txt).split())
+
+all_tweets_no_urls = [remove_url(tweet) for tweet in all_tweets]
+print(f'tweets with no urls {all_tweets_no_urls[:5]}')
